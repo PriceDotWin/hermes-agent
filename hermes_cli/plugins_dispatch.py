9bddc55d0566ed393f3e22e66d60061aae9b3348
KoNit-K <konit.block@protonmail.com>
2026-09-13 12:57:15 +0800

    fix(plugins): fail closed on policy hook exceptions
    A `pre_tool_call` guard that raised failed OPEN (only `_report_hook_failure`,
    results stayed empty, the tool ran) while one that timed out failed CLOSED with
    a block directive. For a veto hook a crashing guard is the control quietly
    disappearing; make both failure modes of `_HOOK_TIMEOUT_FAIL_CLOSED_HOOKS`
    consistent: an exception appends a block directive in addition to the warning.
    
    Fixes #109624
    Salvages #109632
    
    (cherry picked from commit 6ccf5d8a4082fc6b9af80270aae9f13b0320cfb8)

--- hermes_cli/plugins_dispatch.py
+++ hermes_cli/plugins_dispatch.py
@@ -220,6 +220,8 @@ class PluginDispatchMixin:
                     results.append(ret)
             except (Exception, SystemExit) as exc:
                 self._report_hook_failure(hook_name, cb, kwargs, exc)
+                if fail_closed:
+                    results.append({"action": "block", "message": _PRE_TOOL_CALL_TIMEOUT_BLOCK_MESSAGE})
         return results
 
     def _report_hook_failure(
